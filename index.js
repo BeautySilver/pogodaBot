784a4cd093b27a5dab595e5094cc2a2f9c9593db
BeautySilver <gubar1234567@gmail.com>
2019-05-09 20:57:18 +0300

    Add some fitches

--- index.js
+++ index.js
@@ -18,15 +18,23 @@ bot.onText(/today/, (msg) => {
   if (err) throw (err);
 
   let $ = cheerio.load(res.body);
-
- // console.log($(".main #bd2").text());
-   let something  = $("#bd1 .temperature ").text();
-   console.log(something);
+   
+   let min_max  = $("#bd1 .temperature ").text();
+   let current_temperature =  $(".today-temp").text(); 
+   let temperature = [$(".weatherDetails .temperature .p1").text(), "   ", $(".weatherDetails .temperature .p2").text(), "  " , $(".weatherDetails .temperature .p3").text(),"   ",$(".weatherDetails .temperature .p4").text(),"    ",$(".weatherDetails .temperature .p5").text(),"     ",$(".weatherDetails .temperature .p6").text(),"     ",$(".weatherDetails .temperature .p7").text(),"    ",$(".weatherDetails .temperature .p8").text()].join("");
+   let temperatureSens = [$(".weatherDetails .temperatureSens .p1").text(), "   ", $(".weatherDetails .temperatureSens .p2").text(), "  " , $(".weatherDetails .temperatureSens .p3").text(),"   ",$(".weatherDetails .temperatureSens .p4").text(),"    ",$(".weatherDetails .temperatureSens .p5").text(),"     ",$(".weatherDetails .temperatureSens .p6").text(),"     ",$(".weatherDetails .temperatureSens .p7").text(),"    ",$(".weatherDetails .temperatureSens .p8").text()].join("");
+  let atmosphere_pressure = $(".gray").text();
+ let array =  atmosphere_pressure.split("  ");
+    console.log(min_max);
     const chatId = msg.chat.id;
-  bot.sendMessage(chatId, something);
+  bot.sendMessage(chatId, "Today the temperature:" + min_max);
+  bot.sendMessage(chatId, "Current temperature: " + current_temperature );
+  bot.sendMessage(chatId, "All details"+`\n`+"0:00 3:00 6:00 9:00 12:00 15:00 18:00 21:00"+`\n`+temperature+`\n`+temperatureSens);
+  bot.sendMessage(chatId,"Atmosphere pressure for all day:"+array[0,8])
 })
   
 });
+
 
 bot.onText(/tomorrow/, (msg) => {
 	needle.get(URL,  function(err, res){
@@ -34,30 +42,15 @@ bot.onText(/tomorrow/, (msg) => {
 
   let $ = cheerio.load(res.body);
 
- // console.log($(".main #bd2").text());
-   let something  = $("#bd2 .temperature ").text();
-   console.log(something);
+   let min_max  = $("#bd1 .temperature ").text();
+   let current_temperature =  $(".today-temp").text();
+   console.log(min_max);
     const chatId = msg.chat.id;
-  bot.sendMessage(chatId, something);
+  bot.sendMessage(chatId, "Tomorrow the temperature is" + min_max);
 })
   
 });
 
-
-bot.onText(/next/, (msg) => {
-	needle.get(URL,  function(err, res){
-  if (err) throw (err);
-
-  let $ = cheerio.load(res.body);
-
- // console.log($(".main #bd2").text());
-   let something  = $("#bd3 .temperature ").text();
-   console.log(something);
-    const chatId = msg.chat.id;
-  bot.sendMessage(chatId, something);
-})
-  
-});
 
 
 
